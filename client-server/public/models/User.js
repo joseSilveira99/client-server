97cd345bf7e271ef8aa6b7402ab73abda6660e7a
joseSilveira99 <42976957+joseSilveira99@users.noreply.github.com>
2019-01-10 22:47:49 -0200

    Instalaçao
    npm install para dependencia e instalar o bower para estilos

---
+++ client-server/public/models/User.js
@@ -0,0 +1,176 @@
+class User{
+    constructor(name, gender, birth, country, email, password, photo, admin){
+        this._id;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
+    }
+    get id(){
+        return this._id;
+    }
+    get register(){
+        return this._register;
+    }
+    set register(value){
+        this._register = value;
+    }
+    get name(){
+        return this._name;
+    }
+    set name(value){
+        this._name = value;
+    }
+    get gender(){
+        return this._gender;
+    }
+    set gender(value){
+        this._gender = value;
+    }
+    get birth(){
+        return this._birth;
+    }
+    set birth(value){
+        this._birth = value;
+    }
+    get country(){
+        return this._country;
+    }
+    set country(value){
+        this._country = value;
+    }
+    get email(){
+        return this._email;
+    }
+    set email(value){
+        this._email = value;
+    }
+    get password(){
+        return this._password;
+    }
+    set password(value){
+        this._password = value;
+    }
+    get photo(){
+        return this._photo;
+    }
+    set photo(value){
+        this._photo = value;
+    }
+    get admin(){
+        return this._admin;
+    }
+    set admin(value){
+        this._admin = value;
+    }
+    loadFromJSON(json){
+        for(let name in json){//para cada nome encontrado no json fazer...
+            switch(name){
+                case '_register':
+                this[name] = new Date(json[name]);
+                break;
+                default:
+                if(name.substring(0, 1) === '_') this[name] = json[name];
+            }
+        }
+    }
+    static getUsersStorage(){
+      //return   HttpRequest.get('/users');
+      return   Fetch.get('/users');
+    }
+
+    toJSON(){
+      let json = {};
+      Object.keys(this).forEach(key=>{
+        if(this[key] != undefined) json[key] = this[key];
+      });
+      return json;
+    }
+    save(){
+      return new Promise((resolve, reject)=>{
+        let promise;
+        if(this.id){
+          //promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+          promise = Fetch.put(`/users/${this.id}`, this.toJSON());
+        }else{
+          //promise = HttpRequest.post(`/users`, this.toJSON());
+          promise = Fetch.post(`/users`, this.toJSON());
+        }
+        promise.then(data=>{
+          this.loadFromJSON(data);
+          resolve(this);
+        }).catch(e=>{
+          reject(e);
+        });
+      });
+    }
+    remove(){
+      //return HttpRequest.delete(`/users/${this.id}`);
+      return Fetch.delete(`/users/${this.id}`);
+    }
+}
+
+/*
+    loadFromJSON(json){
+        for(let name in json){//para cada nome encontrado no json fazer...
+            switch(name){
+                case '_register':
+                this[name] = new Date(json[name]);
+                break;
+                default:
+                this[name] = json[name];
+            }
+        }
+    }
+    static getUsersStorage(){
+        let users = [];
+        if(localStorage.getItem('users')){
+            users = JSON.parse(localStorage.getItem('users'));
+        }
+        return users;
+    }
+    getNewId(){
+        let usersId = parseInt(localStorage.getItem('usersId'));
+        if(!usersId > 0) usersId = 0;
+
+        usersId++;
+
+        localStorage.setItem('usersId', usersId);
+
+        return usersId;
+    }
+    save(){
+        let users = User.getUsersStorage();//retorna todos os usuario que estao no localStorage e ira criar um array
+
+        if(this.id > 0){
+            users.map(u=>{
+                if(u._id == this.id){
+                    Object.assign(u, this)
+                }
+                return u;
+            });
+
+        }else{
+            this._id = this.getNewId();
+            users.push(this);
+        }
+        localStorage.setItem('users', JSON.stringify(users));
+
+    }
+    remove(){
+        let users = User.getUsersStorage();
+
+        users.forEach((userData, index)=>{
+            if(this._id == userData._id){
+                users.splice(index, 1);//users é o array do localStorage, splice remove do localStorage
+            }
+        });
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+}
+*/
